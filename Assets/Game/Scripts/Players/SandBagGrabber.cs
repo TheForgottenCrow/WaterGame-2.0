56f514a43e0ec85d8d42563e1bdbbe3b75b51e9d
Unknown <davidjejans@hotmail.nl>
2019-01-06 23:48:14 +0100

    Little improvement on sandbag holding

--- Assets/Game/Scripts/Players/SandBagGrabber.cs
+++ Assets/Game/Scripts/Players/SandBagGrabber.cs
@@ -7,7 +7,7 @@ public class SandBagGrabber : MonoBehaviour {
 	public bool m_CurrentlyHolding;
 	private Transform m_SandBagPosition;
 	private BoxCollider m_SandBagCollider;
-	private SandBag m_SandBagHolding;
+	public SandBag m_SandBagHolding;
 
 	private void Update()
 	{
@@ -21,44 +21,46 @@ public class SandBagGrabber : MonoBehaviour {
 
 	private void OnTriggerStay(Collider collision)
 	{
-		if (collision.gameObject.CompareTag("SandBag"))
+		if (Input.GetKeyDown(m_Interact))
 		{
-			m_SandBagHolding = collision.gameObject.GetComponent<SandBag>();
+			if (collision.gameObject.CompareTag("SandBag"))
+			{
+				m_SandBagHolding = collision.gameObject.GetComponent<SandBag>();
 
-			if (Input.GetKeyDown(m_Interact) && m_SandBagHolding.m_PickedUp == false && m_CurrentlyHolding == false)
-			{
-				m_SandBagPosition = collision.gameObject.GetComponent<Transform>();
-				m_SandBagPosition.parent = transform.parent;
-				m_SandBagCollider = m_SandBagPosition.GetComponent<BoxCollider>();
-				m_SandBagCollider.isTrigger = false;
-				m_SandBagHolding.m_PickedUp = true;
-				m_CurrentlyHolding = true;
+				if (m_SandBagHolding.m_PickedUp == false && m_CurrentlyHolding == false)
+				{
+					m_SandBagPosition = collision.gameObject.GetComponent<Transform>();
+					m_SandBagPosition.parent = transform.parent;
+					m_SandBagCollider = m_SandBagPosition.GetComponent<BoxCollider>();
+					m_SandBagCollider.isTrigger = true;
+					m_SandBagHolding.m_PickedUp = true;
+					m_CurrentlyHolding = true;
+				}
+				else if(m_SandBagHolding.m_PickedUp == true && m_CurrentlyHolding == true)
+				{
+					m_SandBagHolding.m_PickedUp = false;
+					m_CurrentlyHolding = false;
+					m_SandBagCollider.isTrigger = false;
+					m_SandBagPosition.parent = null;
+					m_SandBagPosition = null;
+					m_SandBagCollider = null;
+					m_SandBagHolding = null;
+				}
 			}
-			else if (Input.GetKeyDown(m_Interact) && m_SandBagHolding.m_PickedUp == true && m_CurrentlyHolding == true)
+			else if (collision.gameObject.CompareTag("SpawnerCrate"))
 			{
-				m_SandBagHolding.m_PickedUp = false;
-				m_CurrentlyHolding = false;
-				m_SandBagCollider.isTrigger = true;
-				m_SandBagPosition.parent = null;
-				m_SandBagPosition = null;
-				m_SandBagCollider = null;
-				m_SandBagHolding = null;
-			}
-		}
-		else if (collision.gameObject.CompareTag("SpawnerCrate"))
-		{
-			SpawnerBlock sandBagSpawner = collision.gameObject.GetComponent<SpawnerBlock>();
+				SpawnerBlock sandBagSpawner = collision.gameObject.GetComponent<SpawnerBlock>();
 
-			if (Input.GetKeyDown(m_Interact) &&  m_CurrentlyHolding == false && sandBagSpawner.m_SandBagsInCrate > 0)
-			{
-				Transform playerTransform = this.GetComponent<Transform>();
-				m_SandBagHolding = sandBagSpawner.TakeFromCrate(playerTransform);
-				m_SandBagPosition = m_SandBagHolding.gameObject.GetComponent<Transform>();
-				m_SandBagPosition.parent = transform.parent;
-				m_SandBagCollider = m_SandBagPosition.GetComponent<BoxCollider>();
-				m_SandBagCollider.isTrigger = false;
-				m_SandBagHolding.m_PickedUp = true;
-				m_CurrentlyHolding = true;
+				if (m_CurrentlyHolding == false && sandBagSpawner.m_SandBagsInCrate > 0)
+				{
+					m_SandBagHolding = sandBagSpawner.TakeFromCrate(transform);
+					m_SandBagPosition = m_SandBagHolding.gameObject.GetComponent<Transform>();
+					m_SandBagPosition.parent = transform.parent;
+					m_SandBagCollider = m_SandBagPosition.GetComponent<BoxCollider>();
+					m_SandBagCollider.isTrigger = true;
+					m_SandBagHolding.m_PickedUp = true;
+					m_CurrentlyHolding = true;
+				}
 			}
 		}
 	}
