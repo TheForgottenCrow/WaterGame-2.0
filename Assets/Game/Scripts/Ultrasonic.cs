e723366c4707d5fd2e8f1a065849984c4d3d80ca
Unknown <emerson_10@hotmail.nl>
2019-01-25 15:54:08 +0100

    fixed multie threading

--- Assets/Game/Scripts/Ultrasonic.cs
+++ Assets/Game/Scripts/Ultrasonic.cs
@@ -1,37 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 using System.IO.Ports;
+using System;
+using System.Threading;
+
 
 public class Ultrasonic : MonoBehaviour
 {
 	[SerializeField] private SpawnerBlock m_SpawnerBlock;
     //get the serial port
-    SerialPort m_SerialPort;
+    private Thread thread1;
 
 
     // Use this for initialization
     void Start()
     {
         //zoek voor de juiste COM (check in arduino code welke het is en pas de COM aan naar de gene die dr staat)
-        m_SerialPort = new SerialPort("COM6", 9600);
+        ThreadWork.m_SerialPort = new SerialPort("COM4", 9600);
         //arduino openen
-        m_SerialPort.Open();
-        m_SerialPort.DiscardInBuffer();
+        
+
+        thread1 = new Thread(ThreadWork.DoWork);
+        thread1.Start();
+
+        //StartCoroutine(ReadAsync());
     }
 
     void Update()
     {
-        //if statement die infrarood terug geeft 
-        String read = m_SerialPort.ReadLine();
-        if (read == "true")
+        
+    }
+
+    private void OnDestroy()
+    {
+        thread1.Abort();
+    }
+
+}
+
+public class ThreadWork
+{
+    public static bool interrupted;
+    public static SerialPort m_SerialPort;
+
+    public static void DoWork()
+    {
+        m_SerialPort.Open();
+        m_SerialPort.DiscardInBuffer();
+        while (true)
         {
-			m_SpawnerBlock.m_Interupted = true;
+            String read = m_SerialPort.ReadLine();
+            if (read == "true")
+            {
+                interrupted = true;
+                Debug.Log("1");
+            }
+            else if (read == "false")
+            {
+                interrupted = false;
+                Thread.Sleep(5);
+            }
         }
-        else if (read == "false")
-        {
-			m_SpawnerBlock.m_Interupted = false;
-		}
     }
 }
